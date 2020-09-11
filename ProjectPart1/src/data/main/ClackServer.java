72e71bf5fa483505ae43bd867900cd32506390ea
ColinMcDonough <70282607+ColinMcDonough@users.noreply.github.com>
2020-09-11 16:39:35 -0400

    Update ClackServer.java

--- ProjectPart1/src/data/main/ClackServer.java
+++ ProjectPart1/src/data/main/ClackServer.java
@@ -1,9 +1,18 @@
 package main;
 
 import data.ClackData;
-
+/**
+ * Creates a Server class with data 
+ * @author Matthew Frommeyer & Colin McDonough
+ * 
+ *
+ */
 public class ClackServer{
-
+	/** private data for port, closeConnection, dataToReceiveFromClient,
+	 * dataToSendToClient and DEFAULT_PORT_NUMBER
+	 * 
+	 * 
+	 */
 	private int port;
 	private boolean closeConnection;
 	private ClackClient dataToRecieveFromClient;
@@ -12,6 +21,10 @@ public class ClackServer{
 	private static final int DEFAULT_PORT_NUMBER = 7000;
 	
 	//constructor with port provided as input
+	/**
+	 * Creates ClackServer with given port
+	 * @param port
+	 */
 	public ClackServer(int port) {
 		this.port = port;
 		this.closeConnection = true;
@@ -19,12 +32,18 @@ public class ClackServer{
 		this.dataToSendToClient = null;
 	}
 	//basic constructor for default values
+	/**
+	 * Creates ClackServer without a port
+	 */
 	public ClackServer() {
 		this(DEFAULT_PORT_NUMBER);
 		this.closeConnection = true;
 		this.dataToRecieveFromClient = null;
 		this.dataToSendToClient = null;
 	}
+	/**
+	 * Next 3 methods do nothing at the moment
+	 */
 	//doesnt do anything yet
 	public void start() {
 		
@@ -38,12 +57,19 @@ public class ClackServer{
 		
 	}
 	//return port number	
+	/**
+	 * returns port number
+	 * @return
+	 */
 	public int getPort() {
 		return port;
 	}
 	
 	@Override
-	//overwrite hashcode
+	//override hashcode
+	/**
+	 * Overrides hashcode
+	 */
 	public int hashCode() {
 		int result = 5;
 		result = (7*result) + port;
@@ -52,7 +78,10 @@ public class ClackServer{
 		result = (7*result) + ((dataToSendToClient == null) ? 0 : dataToSendToClient.hashCode());
 		return result;
 	}
-	//overwrite equals 
+	//override equals
+	/**
+	 * Overrides equals
+	 */
 	public boolean equals(Object other) {
 		ClackServer otherClackServer = (ClackServer)other;
 		return this.port == otherClackServer.port &&
@@ -61,7 +90,10 @@ public class ClackServer{
 		 this.dataToSendToClient == otherClackServer.dataToSendToClient;
 			
 	}
-	//overwrites toString
+	//overrides toString
+	/**
+	 * Overrides toString
+	 */
 	public String toString() {
 		return "The port number is: " + this.port + "\n" + 
 			   "The closeConnection is: " + this.closeConnection + "\n" + 
