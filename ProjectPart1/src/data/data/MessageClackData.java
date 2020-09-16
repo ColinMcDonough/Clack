3a4e26fc8e8102dc185f56e0d0e46c373e2dd836
ColinMcDonough <70282607+ColinMcDonough@users.noreply.github.com>
2020-09-16 16:11:19 -0400

    Add files via upload

--- ProjectPart1/src/data/data/MessageClackData.java
+++ ProjectPart1/src/data/data/MessageClackData.java
@@ -1,71 +1,38 @@
 package data;
 
-/**
- * Class MessageClackData that extends ClackData
- * @author Matthew Frommeyer & Colin McDonough
- *
- */
-
 public class MessageClackData extends ClackData{
 	 
-	/*
-	 * Private Data type that holds the message
-	 */
+	//data types
 	private String message;
 
-	/**
-	 * Constructor that takes as input a userName, message and type
-	 * @param userName
-	 * @param message
-	 * @param type
-	 */
+	// Constructors
 	public MessageClackData(String userName, String message, int type){
 		super(userName, type);
 		this.message = message;
 	}
-	/**
-	 * Default constructor that sets the object to default values
-	 */
+	
 	public MessageClackData(){
 		super();
-		this.message = null;
+		this.message = "Null";
 	}
-	/**
-	 * returns the message
-	 */
+	
 	public String getData() {
 		return message;
 	}
-	/**
-	 * overrides the hashCode, equals and toString methods
-	 */
+	
 	@Override
-	/**
-	 * overrides hashCode
-	 */
 	public int hashCode(){
 		int result = 5;
-		result = (7*result) + ((message == null) ? 1 : 0);
-		result = (7*result) + ((getUserName() == "Anon") ? 1 : 0);
-		result = (7*result) + getType();
+		result = (7*result) + message.hashCode();
 		return result;
 	}
-	/**
-	 * overrides equals
-	 */
+	
 	public boolean equals(Object other) {
 		MessageClackData otherMessageClackData = (MessageClackData)other;
-		return this.message == otherMessageClackData.message &&
-				this.getUserName() == otherMessageClackData.getUserName() &&
-				this.getType() == otherMessageClackData.getType();
+		return this.message == otherMessageClackData.message;
 	}
-	/**
-	 * overrides toString
-	 */
+	
 	public String toString() {
-		return "The message is: " + this.message +  "\n" +
-				"The userName is: " + this.getUserName() + "\n" +
-				"The type is: " + this.getType() + "\n" +
-				"The date is: " + this.getDate();
+		return "The message is: " + this.message + "\n\n";
 	}
 }
---
+++ ProjectPart1/src/data/data/TestClackData.java
@@ -0,0 +1,138 @@
+package test;
+
+import data.ClackData;
+import data.FileClackData;
+import data.MessageClackData;
+/**
+ * Test class for the ClackData class
+ * @author Matthew Frommeyer & Colin Mcdonough
+ *
+ */
+public class TestClackData {
+	
+	public static void main(String[] args) {
+		/**
+		 * Creates a message with a username, message, and type
+		 * then outputs the data
+		 */
+		ClackData mclackdata1 = new MessageClackData("Matt", "hello", 2);
+		System.out.println("This is mclackdata1.");
+		System.out.println(mclackdata1);
+		System.out.println(mclackdata1.hashCode());
+		
+		System.out.println();
+		/**
+		 * Creates a message with no given arguments
+		 * then outputs the data
+		 */
+		ClackData mclackdata2 = new MessageClackData();
+		System.out.println("This is mclackdata2.");
+		System.out.println(mclackdata2);
+		System.out.println(mclackdata2.hashCode());
+		
+		System.out.println();
+		/**
+		 * Creates a message with no given arguments
+		 * then outputs the data
+		 */
+		ClackData mclackdata3 = new MessageClackData();
+		System.out.println("This is mclackdata3.");
+		System.out.println(mclackdata3);
+		System.out.println(mclackdata3.hashCode());
+		
+		System.out.println();
+		
+		System.out.println("Test if mclackdata2 is the same as mclackdata3");
+		/**
+		 * Tests to see if they are equal given both are created using default constructors
+		 */
+		if(mclackdata2.equals(mclackdata3)) {
+			System.out.println("They are the same!");
+		}else {
+			System.out.println("They are not the same!");
+		}
+		
+		System.out.println();
+		
+		System.out.println("Testing if mclackdata1 is the same as mclackdata2.");
+		/**
+		 * Tests to see if mclackdata1 is equal to mclackdata2 
+		 */
+		if(mclackdata1.equals(mclackdata2)) {
+			System.out.println("They are the same!");
+		}else {
+			System.out.println("They are not the same!");
+		}
+		
+		System.out.println();
+		/**
+		 *  Tests getdata 
+		 */
+		System.out.println("Test getData function on mclackdata1");
+		//test getData
+		System.out.println("getData() returns: " + mclackdata1.getData());
+		
+		System.out.println();
+		/**
+		 * Tests FileClackData with two using default constructors and the other one using the given arguments
+		 */
+		//test fileClackData
+		ClackData fclackdata1 = new FileClackData("Matt", "hello.txt", 2);
+		System.out.println("This is fclackdata1.");
+		System.out.println(fclackdata1);
+		System.out.println(fclackdata1.hashCode());
+		
+		System.out.println();
+		
+		ClackData fclackdata2 = new FileClackData();
+		System.out.println("This is fclackdata2.");
+		System.out.println(fclackdata2);
+		System.out.println(fclackdata2.hashCode());
+		
+		System.out.println();
+		
+		ClackData fclackdata3 = new FileClackData();
+		System.out.println("This is fclackdata3.");
+		System.out.println(fclackdata3);
+		System.out.println(fclackdata3.hashCode());
+		
+		System.out.println();
+		
+		System.out.println("Test if fclackdata2 is the same as fclackdata3");
+		/**
+		 * Checks to see if both made with defaults are the same
+		 */
+		if(fclackdata2.equals(fclackdata3)) {
+			System.out.println("They are the same!");
+		}else {
+			System.out.println("They are not the same!");
+		}
+		
+		System.out.println();
+		/**
+		 * Renames fclackdata2's filename then checks if they are still the same as fclackdata3
+		 */
+		System.out.println("Renaming fclackdata2's fileName to hello.txt.");
+		((FileClackData) fclackdata2).setFileName("hello.txt");
+		System.out.println("fclackdata2's new fileName is: " + ((FileClackData) fclackdata2).getFileName());
+		
+		System.out.println();
+		
+		System.out.println("Testing if fclackdata2 is the same as fclackdata3 after the change.");
+		
+		if(fclackdata2.equals(fclackdata3)) {
+			System.out.println("They are the same!");
+		}else {
+			System.out.println("They are not the same!");
+		}
+		
+		System.out.println();
+		/**
+		 * Tests get data on fclackdata1
+		 */
+		System.out.println("Test getData function on fclackdata1");
+		//test getData
+		System.out.println("getData() returns: " + fclackdata1.getData());
+	}
+
+}
