7e281c29000f8529d359e7ffd1dfb11f5e0b7f26
ColinMcDonough <70282607+ColinMcDonough@users.noreply.github.com>
2020-09-20 18:51:04 -0400

    Add files via upload

--- ProjectPart1/src/data/data/MessageClackData.java
+++ ProjectPart1/src/data/data/MessageClackData.java
@@ -1,38 +1,75 @@
 package data;
 
+/**
+ * Class MessageClackData that extends ClackData
+ * @author Matthew Frommeyer + Colin McDonough
+ *
+ */
+
 public class MessageClackData extends ClackData{
 	 
-	//data types
+	/*
+	 * Private Data type that holds the message
+	 */
 	private String message;
 
-	// Constructors
+	/**
+	 * Constructor that takes as input a userName, message and type
+	 * @param userName
+	 * @param message
+	 * @param type
+	 */
+	public MessageClackData(String userName, String message, String key, int type) {
+		super(userName, type);
+		this.message = encrypt(message, key);
+	}
 	public MessageClackData(String userName, String message, int type){
 		super(userName, type);
 		this.message = message;
 	}
-	
+	/**
+	 * Default constructor that sets the object to default values
+	 */
 	public MessageClackData(){
 		super();
-		this.message = "Null";
+		this.message = null;
 	}
-	
+	/**
+	 * returns the message
+	 */
 	public String getData() {
-		return message;
+		return decrypt(message, key);
 	}
-	
+	/**
+	 * overrides the hashCode, equals and toString methods
+	 */
 	@Override
+	/**
+	 * overrides hashCode
+	 */
 	public int hashCode(){
 		int result = 5;
-		result = (7*result) + message.hashCode();
+		result = (7*result) + ((message == null) ? 1 : 0);
+		result = (7*result) + ((getUserName() == "Anon") ? 1 : 0);
+		result = (7*result) + getType();
 		return result;
 	}
-	
+	/**
+	 * overrides equals
+	 */
 	public boolean equals(Object other) {
 		MessageClackData otherMessageClackData = (MessageClackData)other;
-		return this.message == otherMessageClackData.message;
+		return this.message == otherMessageClackData.message &&
+				this.getUserName() == otherMessageClackData.getUserName() &&
+				this.getType() == otherMessageClackData.getType();
 	}
-	
+	/**
+	 * overrides toString
+	 */
 	public String toString() {
-		return "The message is: " + this.message + "\n\n";
+		return "The message is: " + this.message +  "\n" +
+				"The userName is: " + this.getUserName() + "\n" +
+				"The type is: " + this.getType() + "\n" +
+				"The date is: " + this.getDate();
 	}
 }
